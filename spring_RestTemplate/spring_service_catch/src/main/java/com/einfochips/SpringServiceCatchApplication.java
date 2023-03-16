80aa556bc826dc51cc00ccabae62068538285740
Jaydeep zanzmera <Jaydeep.zanzmera@einfochips.com>
2023-03-16 23:13:55 +0530

    commit for assingment spring Rest Template

---
+++ spring_RestTemplate/spring_service_catch/src/main/java/com/einfochips/SpringServiceCatchApplication.java
@@ -0,0 +1,13 @@
+package com.einfochips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServiceCatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServiceCatchApplication.class, args);
+	}
+
+}
